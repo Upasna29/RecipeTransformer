03d2bf18bf86340708a0d0a755b660d18eb820f7
Collin Pham <collinpham2018@u.northwestern.edu>
2017-03-09 17:45:39 -0600

    Ingredients done

--- components/Ingredients.py
+++ components/Ingredients.py
@@ -23,49 +23,63 @@ ingredients = [
   u'Add all ingredients to list'
 ]
 
+measurments = ['tablespoons', 'cloves', 'teaspoons', 'inch', 'ounce', 'piece', 'can']
+
 ## determineQuantity
 # string -> Arr of Str
 #
 # determineQuantity takes a ingredient string and parses out the quantities to determine the quantity
 
-def determineIngredients(ingredient):
-  quantities = []
-  measurments = []
-  ingredient_names = []
-  measurment_flag = False
+def determineIngredients(instruction):
+  # Initialize items
+  ingredient = []
+  quantity = []
+  measurment = []
+  preperation = []
   
-  ingredient_names = ingredient.split(' ')
-  print ingredient
-  numerics, ingredient_names = parseIngredients(ingredient_names)
+  words = instruction.split(' ')
+  print instruction
 
+  # split 
+  # quantity_and_measurement, ingredient_and_preperation = parseIngredients(words)
+  # print quantity_and_measurement
+  # print ingredient_and_preperation
 
-  for word in numerics:
-    if measurment_flag:
-      measurments.append(word)
-      measurment_flag = False
+  # extract measurments
+  for word in words:
+    if word in measurments:
+      if not measurment:
+        measurment = word
+      instruction = removeWord(instruction, word)
+    elif word.replace(')', '') in measurments:
+      measurment = word
+      instruction = removeWord(instruction, measurment)
+
+  if ')' in measurment:
+    measurment = measurment.replace(')', '')
+  # extract quantity
+  for word in words:
     for letter in word:
       if letter.isdigit():
-        quantities.append(word)
-        measurment_flag = True
+        quantity.append(word)
+        instruction = removeWord(instruction, word)
         break
 
-  # for word in ingredient:
+  for num in quantity:
+    if '(' in num:
+      quantity = num.replace('(', '')
 
+  ingredient, preperation = filterIngredient(instruction)
 
+  if ',' in ingredient:
+    ingredient = ingredient.replace(',', '')
 
-  quantities = filterQuantity(quantities)
-  measurments = filterMeasurment(measurments)
-  ingredient_names = filterIngredient(ingredient_names)
-  if quantities or measurments:
-    print ''
-    print 'quantity'
-    print quantities
-    print ''
-    print 'measurment'
-    print measurments
-    print ''
-    print 'ingredient'
-    print ingredient_names
+  print 'The quantity is: ' + ' '.join(quantity) if isinstance(quantity, list) else 'The quantity is: ' + ''.join(quantity)
+  print 'The measurment is: ' + measurment if measurment else 'No measurment'
+  print 'The ingredient is: ' + ingredient
+  print 'The preperation is: ' + preperation if preperation else 'No preperation'
+  print ''
+
 
 def filterQuantity(quantities):
   correct_quantities = []
@@ -75,7 +89,7 @@ def filterQuantity(quantities):
       correct_quantities = [number[1:]]
     else:
       correct_quantities.append(number)
-  return correct_quantities
+  return ' '.join(correct_quantities)
 
 
 def filterMeasurment(measurments):
@@ -94,24 +108,29 @@ def filterMeasurment(measurments):
 def filterIngredient(ingredient):
   for i in range(0, len(ingredient)):
     if ',' in ingredient[i]:
-      return ingredient[:(i+1)]
-  return ingredient
+      return ingredient[:(i+1)], ingredient[(i+1):]
+  return ingredient, False
 
 
 
 def num_there(s):
     return any(i.isdigit() for i in s)
 
+def removeWord(s, r):
+  return ' '.join([word for word in s.split(' ') if not (word == r)])
 
+# for the instruction, if there is a number, call it the number index.
+#
 def parseIngredients(ingredient):
   number_index = 0
   for i in range(0, len(ingredient)):
     if ')' in ingredient[i]:
+      print ingredient[:(i+2)], ingredient[(i+2):]
       return ingredient[:(i+2)], ingredient[(i+2):]
     elif num_there(ingredient[i]):
       number_index = i
+  print ingredient[:(number_index+2)], ingredient[(number_index+2):]
   return ingredient[:(number_index+2)], ingredient[(number_index+2):]
-
 
 
 
