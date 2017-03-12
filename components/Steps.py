2d4f78c82461c98bc3b3140ece7f249d56781e7b
cmurray7 <cmurray7@users.noreply.github.com>
2017-03-12 17:08:54 -0500

    Commit before pull of new main function

--- components/Steps.py
+++ components/Steps.py
@@ -4,18 +4,13 @@ import WebScraper
 import Ingredients
 from pyStatParser.stat_parser import Parser
 
-# ingredients = ['potatoes', 'water', 'vegetable', 'oil', 'onion',
-#                            'garlic', 'cumin', 'cayenne', 'pepper', 'curry',
-#                            'powder', 'garam', 'masala', 'ginger', 'salt',
-#                            'tomatoes', 'garbanzo', 'beans', 'peas', 'coconut',
-#                            'milk']
-
-# ingredients = ['olive', 'oil', 'cloves', 'garlic', 'hot', 'red', 'pepper', 'flakes', 'chicken', 'breast',
-            #    'marinara', 'sauce', 'basil', 'mozzarella', 'cheese', 'parmesan', 'croutons']
 
 utensils = ['pot', 'pan', 'skillet', 'sheet', 'dish', 'oven', 'rice cooker', 'pressure', 'cooker'
             'grater', 'whisk', 'spoon', 'fork', 'knife', 'tongs', 'spatula', 'peeler', 'thermometer', 'funnel',
-            'knife', 'baking', 'dish']
+            'knife', 'baking', 'dish', 'press', 'grater', 'scissors', 'mortar', 'pestle', 'slotted spoon', 'pin',
+            'measuring', 'zester', 'funnel', 'sink', 'cakepan', 'board', 'strainer', 'tray', 'blender', 'mitts',
+            'bowl', 'grill', 'nutcracker', 'ladle', 'cutter', 'whisk', 'scoop', 'griddle', 'cleaver', 'masher',
+            'colander', 'dutch']
 
 def getListIngredients():
     '''
@@ -59,42 +54,10 @@ def getDirections():
     nested_directions = [re.split('\.|;', direction) for direction in directions]
     steps = [step.lstrip() for direction in nested_directions for step in direction if step]
     return steps
-#
-# def tagSteps(steps):
-#     '''
-#     Given list of "steps", return a part-of-speech-tagged representation of the directions
-#     Note: prepends 'you' onto every phrase prior to tagging to maintain command structure
-#     :param steps:  List of strings where each element is a step/sentence of the directions.
-#     :return: List of List of Tuples of word and part of speech tag
-#     '''
-#     tagged_steps = [None]*len(steps)
-#     for i, step in enumerate(steps):
-#         tokens = nltk.word_tokenize(step)
-#         if step:
-#             tokens.insert(0, u'You')
-#         pos = nltk.pos_tag(tokens)
-#         tagged_steps[i] = pos
-#     return tagged_steps
 
-# def chunkSteps(taggedSteps):
-#     patterns = r"""
-#                 NP: {<DT>*<JJ>*<NN|NNS|NNP|PRP>+}
-#                     {<NP>+<CC|,>+<NP>+}
-#                 VP: {<VB.*><TO>*<IN>*<DT|PDT|RB>*<NP>}
-#                     {<VB.*><PP>}
-#                 PP: {<IN|TO><CD|DT>*<NP>}
-#                 STEP: {<NP><VP>}"""
-#
-#     cp = nltk.RegexpParser(patterns)
-#     chunked_steps = [None]*len(taggedSteps)
-#
-#     for i, step in enumerate(taggedSteps):
-#         chunked_steps[i] = cp.parse(step)
-#
-#     return chunked_steps
 
 def stat_parse(steps):
-    stop_words = ['is', 'and']
+    stop_words = ['is', 'and', 'to', 'time']
 
     vp_steps = []
 
@@ -107,6 +70,9 @@ def stat_parse(steps):
         time_idxs = [idx for idx, word in enumerate(split_steps) if word in ['second', 'seconds', 'minute', 'minutes', 'hour', 'hours']]
         times = [split_steps[idx - 1] + split_steps[idx] for idx in time_idxs]
         step_dict['time'] = times
+
+        tools = [x for x in split_steps if x in utensils]
+        step_dict['tools'] = tools
 
         # Parsing sentence structure from each step and putting in array steps
         parser = Parser()
@@ -123,66 +89,38 @@ def stat_parse(steps):
                     vp_steps.append(i)
                     dict_steps.append(step_dict)
 
+
     for idx, vp_step in enumerate(vp_steps):
         for child in vp_step:
+            print child
             if child.label() == 'VB' or child.label == 'VBP':
                 method = child.leaves()[0]
                 dict_steps[idx]['method'] = method
             else:
                 rest = child.leaves()[1:]
+                rest = [x for x in rest if x not in stop_words]
                 step_ing = [x for x in rest if x.lower() in ingredients]
-                dict_steps[idx]['ingredients'] = step_ing
+                dict_steps[idx]['ingredients'] = list(set(step_ing))
                 pos_rest = child.pos()[1:]
 
-    print dict_steps
+    return dict_steps
+
+def tools(dict_steps):
+    '''
+    Given parsed recipe steps, return all tools involved for recipe
+    :param dict_steps: Dictionary representation of recipe steps as returned by stat_parse
+    :return: List of tools found in recipe.
+    '''
+    tools = []
+    for step in dict_steps:
+        tools = tools + step['tools']
+    return tools
 
 
 
-
-
-
-
-    # def listTools(taggedSteps, ingredients):
-
-
-# '''
-#     Given the directions and ingredients, produce candidates (nouns) for tools needed in recipe
-#     :param taggedSteps: pos-tagged representation of the recipe directions (List of lists)
-#     :param ingredients: list of identity words for recipe ingredients
-#     :return: List of strings -- tools
-#     '''
-#     all_nouns = [None]
-#
-#     for i, step in enumerate(taggedSteps):
-#         nouns = list(filter(lambda x: re.match('NN*', x[1]), step))
-#         nouns = [x[0] for x in nouns]
-#         all_nouns = all_nouns + nouns
-#
-#     all_nouns = set(all_nouns)
-#     all_nouns = [x for x in all_nouns if x]
-#     print all_nouns
-#     not_ingredients = [x for x in all_nouns if x.lower() not in ingredients]
-#     tools = [x for x in not_ingredients if x.lower() in utensils]
-#     return tools
-#
-
-# def getMethods(steps, ingredients):
-#     commands = []
-#
-#     for step in steps:
-#         tokens = nltk.word_tokenize(step)
-#         commands.append(tokens[0])
-#
-#     commands = [x for x in commands if x]
-#     not_ingredients = [x for x in commands if x.lower() not in ingredients]
-#     methods = [x for x in not_ingredients if x.lower() not in utensils]
-#     return methods
-
-
-
-def main():
+def steps():
     steps = getDirections()
-    stat_parse(steps)
-# print getMethods(steps, ingredients)
-# tagged = tagSteps(steps)
-# tools = listTools(tagged, ingredients)
+    dict_steps = stat_parse(steps)
+
+
+
